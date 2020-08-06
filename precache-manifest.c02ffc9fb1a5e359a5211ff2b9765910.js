7bde0b7b3158367d91da0ec063ca94b4e65637c2
Ifrah Saleem <ifrahsaleem123@gmail.com>
2020-08-06 14:34:44 +0500

    Updates

---
+++ precache-manifest.c02ffc9fb1a5e359a5211ff2b9765910.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2d3f5b608e240729bdc66767c005fb",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "c67aecf5981f7750985c",
+    "url": "/todoapp/static/css/main.26c6d0e2.chunk.css"
+  },
+  {
+    "revision": "9f91c0487c5f3a3248f4",
+    "url": "/todoapp/static/js/2.58f23500.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.58f23500.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c67aecf5981f7750985c",
+    "url": "/todoapp/static/js/main.71a1b020.chunk.js"
+  },
+  {
+    "revision": "5a801258d7bf2526530b",
+    "url": "/todoapp/static/js/runtime-main.3e0c5687.js"
+  }
+]);
